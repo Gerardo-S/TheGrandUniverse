662c3cd90a402e954adb0a50e5b2b101a9912d75
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-07-16 18:55:22 -0700

    add env variables

---
+++ APOD-API-Learning/demo.js
@@ -0,0 +1,4 @@
+//  api key @: https://api.nasa.gov/
+// Displays thumbnails of the APODs for the last 16 days (including today) in descending order
+// API Key: apiKey
+// URL Example: https://api.nasa.gov/planetary/apod?api_key=apiKey
