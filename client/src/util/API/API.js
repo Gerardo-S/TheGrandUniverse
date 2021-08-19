400c3a69145bb3a154531571c325bb309c567b64
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-08-19 00:32:31 -0700

    removed unnecessary features

--- client/src/util/API/API.js
+++ client/src/util/API/API.js
@@ -1,19 +1,16 @@
-import axios from "axios";
+require("dotenv").config();
 
+const API_KEY = process.env.REACT_APP_APIKEY;
 
-function fetchApodImages(){
-    return axios.get("/api/apodImages")
+export const makeAPODUrl = (params) => {
+  const url = new URL(
+    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&thumbs=True`
+  );
+  Object.keys(params).forEach((key) => {
+    if (params[key] !== undefined) {
+      url.searchParams.append(key, params[key]);
+    }
+  });
+
+  return url.toString();
 };
-
-function saveApod (apod) {
-    return axios.post("/api/apodImages", apod)
-}
-
-function fetchSavedApod () {
-    return axios.get("/api/SavedapodImages")
-}
-
-function deleteSavedApod () {
-    return axios.delete("/api/SavedapodImages")
-}
-export {fetchApodImages, saveApod, fetchSavedApod,deleteSavedApod};
--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Apod: require("./apod")
-};
